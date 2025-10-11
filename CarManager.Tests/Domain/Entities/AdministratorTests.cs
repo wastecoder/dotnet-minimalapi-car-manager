d9227f64e07e9e8f8f4508c3a5f2a932225e9e7e
Pedro <pedromendes7182@gmail.com>
2025-10-10 21:11:23 -0300

    test(Administrator): adiciona um teste simples

---
+++ CarManager.Tests/Domain/Entities/AdministratorTests.cs
@@ -0,0 +1,27 @@
+﻿using CarManager.Domain.Entities;
+using CarManager.Domain.Enums;
+
+namespace CarManager.Tests.Domain.Entities;
+
+[TestClass]
+public class AdministratorTests
+{
+    [TestMethod]
+    public void ShouldAllowSetAdministratorProperties()
+    {
+        // Arrange & Act
+        var adm = new Administrator
+        {
+            Id = 1,
+            Email = "teste@teste.com",
+            Password = "teste",
+            Role = AdmRole.Adm
+        };
+
+        // Assert
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("teste", adm.Password);
+        Assert.AreEqual(AdmRole.Adm, adm.Role);
+    }
+}
