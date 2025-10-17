3c5376b6fd6e683c8862c0bdd3fd13d4d43a7163
Pedro <pedromendes7182@gmail.com>
2025-10-17 05:22:04 -0300

    test(Administrator): adiciona mock de service e testes de login via HTTP

---
+++ CarManager.Tests/Mocks/AdministratorServiceMock.cs
@@ -0,0 +1,49 @@
+﻿using CarManager.Domain.DTOs;
+using CarManager.Domain.Entities;
+using CarManager.Domain.Enums;
+using CarManager.Domain.Interfaces;
+
+namespace CarManager.Tests.Mocks;
+
+public class AdministratorServiceMock : IAdministratorService
+{
+    private static List<Administrator> administrators = new List<Administrator>()
+    {
+        new Administrator{
+            Id = 1,
+            Email = "adm@teste.com",
+            Password = "123456",
+            Role = AdmRole.Adm
+        },
+        new Administrator{
+            Id = 2,
+            Email = "editor@teste.com",
+            Password = "123456",
+            Role = AdmRole.Editor
+        }
+    };
+
+    public Administrator? Login(LoginDTO loginDTO)
+    {
+        return administrators.Find(a => a.Email == loginDTO.Email
+                                        && a.Password == loginDTO.Password);
+    }
+
+    public Administrator Add(Administrator administrator)
+    {
+        administrator.Id = administrators.Count + 1;
+        administrators.Add(administrator);
+
+        return administrator;
+    }
+
+    public Administrator? GetById(int id)
+    {
+        return administrators.Find(a => a.Id == id);
+    }
+
+    public List<Administrator> GetAll(int? page, int? pageSize)
+    {
+        return administrators;
+    }
+}
---
+++ CarManager.Tests/Requests/AdministratorRequestTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using CarManager.Domain.DTOs;
+using CarManager.Domain.ModelViews;
+using CarManager.Tests.Helpers;
+
+namespace CarManager.Tests.Requests;
+
+[TestClass]
+public class AdministratorRequestTests
+{
+    [ClassInitialize]
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.ClassInit(testContext);
+    }
+
+    [ClassCleanup(ClassCleanupBehavior.EndOfClass)]
+    public static void ClassCleanup()
+    {
+        Setup.ClassCleanup();
+    }
+
+    [TestMethod]
+    public async Task Should_Allow_Administrator_Login_When_Credentials_Are_Valid()
+    {
+        // Arrange
+        var loginDto = new LoginDTO{
+            Email = "adm@teste.com",
+            Password = "123456"
+        };
+
+        var content = new StringContent(JsonSerializer.Serialize(loginDto), Encoding.UTF8,  "Application/json");
+
+        // Act
+        var response = await Setup.client.PostAsync("/administrators/login", content);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        var result = await response.Content.ReadAsStringAsync();
+        var loggedAdmin = JsonSerializer.Deserialize<AdministratorLoginResponse>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        Assert.IsNotNull(loggedAdmin);
+        Assert.IsNotNull(loggedAdmin.Email);
+        Assert.IsNotNull(loggedAdmin.Role);
+        Assert.IsNotNull(loggedAdmin.Token);
+    }
+}
