ebcb9bce5d132c6e2a8705ed754da5b8b1a1fb00
Pedro <pedromendes7182@gmail.com>
2025-10-17 05:21:15 -0300

    test(Setup): adiciona estrutura base para testes de integração

---
+++ CarManager.Tests/Helpers/Setup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using CarManager.Domain.Interfaces;
+using CarManager.Tests.Mocks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarManager.Tests.Helpers;
+
+public class Setup
+{
+    public const string PORT = "5001";
+    public static TestContext testContext = default!;
+    public static WebApplicationFactory<Startup> http = default!;
+    public static HttpClient client = default!;
+
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.testContext = testContext;
+        Setup.http = new WebApplicationFactory<Startup>();
+
+        Setup.http = Setup.http.WithWebHostBuilder(builder =>
+        {
+            builder.UseSetting("https_port", Setup.PORT).UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped<IAdministratorService, AdministratorServiceMock>();
+            });
+
+        });
+
+        Setup.client = Setup.http.CreateClient();
+    }
+
+    public static void ClassCleanup()
+    {
+        Setup.http.Dispose();
+    }
+}
