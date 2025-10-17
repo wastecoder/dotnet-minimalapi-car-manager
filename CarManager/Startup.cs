c5066e71f58bd71373400aeefa35263e1fed4916
Pedro <pedromendes7182@gmail.com>
2025-10-17 05:21:01 -0300

    refactor(Startup): extrai configurações do Program para Startup

---
+++ CarManager/Startup.cs
@@ -0,0 +1,239 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CarManager.Domain.DTOs;
+using CarManager.Domain.Entities;
+using CarManager.Domain.Enums;
+using CarManager.Domain.Interfaces;
+using CarManager.Domain.ModelViews;
+using CarManager.Domain.Services;
+using CarManager.Infraestructure.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace CarManager;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        jwtKey = Configuration.GetValue<string>("Jwt:Key") ?? throw new ArgumentNullException("Jwt:Key");
+    }
+
+    private string jwtKey;
+    private IConfiguration Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+        });
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateLifetime = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+
+        services.AddAuthorization();
+
+        services.AddScoped<IAdministratorService, AdministratorService>();
+        services.AddScoped<IVehicleService, VehicleService>();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer", // Se colocar maiúsculo, vai precisar colocar "Bearer {token}"
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Insert only your JWT token, without 'Bearer'"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        });
+
+        services.AddDbContext<DatabaseContext>(options =>
+        {
+            options.UseMySql
+            (
+                Configuration.GetConnectionString("MySql"),
+                ServerVersion.AutoDetect(Configuration.GetConnectionString("MySql")) 
+            );
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            #region Home
+            endpoints.MapGet("/", () =>
+            {
+                Console.WriteLine("Accessing API documentation...");
+                return Results.Redirect("/swagger");
+            })
+                .AllowAnonymous()
+                .WithTags("Home");
+            #endregion
+
+            #region Administrators
+            string GenerateJwtToken(Administrator administrator)
+            {
+                if (string.IsNullOrEmpty(jwtKey)) return string.Empty;
+
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>()
+                {
+                    new Claim("Email", administrator.Email),
+                    new Claim("Role", administrator.Role.ToString()),
+                    new Claim(ClaimTypes.Role, administrator.Role.ToString())
+                };
+
+                var token = new JwtSecurityToken(
+                    claims:  claims,
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: credentials
+                );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+
+            ValidationErrors ValidateAdministrator(AdministratorDTO administratorDto)
+            {
+                var validation = new ValidationErrors();
+
+                if (string.IsNullOrEmpty(administratorDto.Email))
+                    validation.Messages.Add("E-mail is required");
+                if (string.IsNullOrEmpty(administratorDto.Password))
+                    validation.Messages.Add("Password is required");
+
+                if (string.IsNullOrEmpty(administratorDto.Role) ||
+                    !Enum.TryParse<AdmRole>(administratorDto.Role, ignoreCase: true, out var parsedRole) ||
+                    !Enum.IsDefined(typeof(AdmRole), parsedRole) ||
+                    parsedRole == AdmRole.None)
+                {
+                    validation.Messages.Add("Acceptable roles are 'Adm' or 'Editor'");
+                }
+
+                return validation;
+            }
+
+            bool HasAdministratorValidationErrors(AdministratorDTO dto, out ValidationErrors errors)
+            {
+                errors = ValidateAdministrator(dto);
+                return errors.Messages.Count > 0;
+            }
+
+            endpoints.MapPost("/administrators/login", (LoginDTO loginDTO, IAdministratorService service) =>
+            {
+                var administrator = service.Login(loginDTO);
+                if (administrator == null)
+                    return Results.Unauthorized();
+
+                string token = GenerateJwtToken(administrator);
+                return Results.Ok(new AdministratorLoginResponse(
+                    administrator.Email,
+                    administrator.Role.ToString(),
+                    token
+                ));
+            })
+                .AllowAnonymous()
+                .WithTags("Administrators");
+
+            endpoints.MapPost("/administrators", ([FromBody] AdministratorDTO administratorDto, IAdministratorService service) =>
+            {
+                if (HasAdministratorValidationErrors(administratorDto, out var errors))
+                    return Results.BadRequest(errors);
+
+                Enum.TryParse<AdmRole>(administratorDto.Role, true, out var parsedRole);
+
+                var administrator = new Administrator
+                {
+                    Email = administratorDto.Email,
+                    Password = administratorDto.Password,
+                    Role = parsedRole
+                };
+                service.Add(administrator);
+
+                return Results.Created($"/administrators/{administrator.Id}", administrator);
+            })
+                .RequireAuthorization()
+                .RequireAuthorization(new AuthorizeAttribute { Roles = "Adm" })
+                .WithTags("Administrators");
+
+            endpoints.MapGet("/administrators", ([FromQuery] int? page, IAdministratorService service) =>
+            {
+                var administrators = service.GetAll(page, 5);
+
+                var administratorResponses = administrators
+                    .Select(a => new AdministratorResponse(a.Id, a.Email, a.Role))
+                    .ToList();
+
+                return Results.Ok(administratorResponses);
+            })
+                .RequireAuthorization()
+                .RequireAuthorization(new AuthorizeAttribute { Roles = "Adm" })
+                .WithTags("Administrators");
+
+            endpoints.MapGet("/administrators/{id:int}", ([FromRoute] int id, IAdministratorService service) =>
+            {
+                var administrator = service.GetById(id);
+                if (administrator == null) return Results.NotFound("Administrator not found");
+
+                var administratorResponse = new AdministratorResponse(
+                    administrator.Id,
+                    administrator.Email,
+                    administrator.Role
+                );
+
+                return Results.Ok(administratorResponse);
+            })
+                .RequireAuthorization()
+                .RequireAuthorization(new AuthorizeAttribute { Roles = "Adm" })
+                .WithTags("Administrators");
+            #endregion
+        });
+    }
+}
